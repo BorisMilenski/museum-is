36bd364b47ce56b9818015eaaa9d469c693f815c
BORIS <boris.milenski@abv.bg>
2022-06-14 12:03:23 +0300

    Initial commit

---
+++ src/main/java/com/borismilenski/museumis/MuseumIsApplication.java
@@ -0,0 +1,13 @@
+package com.borismilenski.museumis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseumIsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MuseumIsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/borismilenski/museumis/MuseumIsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.borismilenski.museumis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumIsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
